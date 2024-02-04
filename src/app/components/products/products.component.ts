215bb90dd334160d9d99fd026a27531b89424dd8
Ahmed-Abdelhamee <medoabdelhameed@gmail.com>
2024-02-04 18:22:22 +0200

    first commit

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss','../../Modal/main-style.css']
 })
 export class ProductsComponent {
 
--- src/app/components/sounds/sounds.component.ts
+++ src/app/components/sounds/sounds.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sounds',
   templateUrl: './sounds.component.html',
-  styleUrls: ['./sounds.component.scss']
+  styleUrls: ['./sounds.component.scss','../../Modal/main-style.css']
 })
 export class SoundsComponent {
 
