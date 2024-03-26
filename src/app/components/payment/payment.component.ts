6e75acfcfaa5552ebd826751fbb9df0b4cb58b46
Ahmed-Abdelhamee <medoabdelhameed@gmail.com>
2024-03-26 13:32:45 +0200

    first committ

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { product } from 'src/app/Modal/interfaces/product.interface';
+import { social } from 'src/app/Modal/interfaces/social.interface';
+import { SocialMediaService } from 'src/app/services/social-media.service';
 
 @Component({
   selector: 'app-payment',
@@ -12,12 +16,38 @@ export class PaymentComponent {
 
   totalCost: number = 0;
 
-  constructor() {
+  whatsappDataLink: product[] = [];
+
+  whatsappDataLinkMsg: string = "";
+
+  whatsapp: social[] = [];
+
+  fakeWhatsappButton: boolean = true;
+  
+  showAlertMsg:boolean=false;
+
+  address = this.formbuilder.group({
+    phone: ["", Validators.required],
+    country: ["", Validators.required],
+    street: ["", Validators.required],
+    area: ["", Validators.required],
+    home: ["", Validators.required],
+  })
+
+
+  constructor(private formbuilder: FormBuilder, private iconsServ: SocialMediaService, private toastr: ToastrService) {
     this.cart = JSON.parse(localStorage.getItem("products-cart")!) ? JSON.parse(localStorage.getItem("products-cart")!) : [];
     for (let item of this.cart) {
       this.totalCost += item.productDiscount * item.productquantity!;
     }
     this.totalCost += 3;
+
+    // ----------------------- get whatsapp -----------------------
+    iconsServ.getSocialAPI("whats").subscribe(data => {
+      for (const key in data) {
+        this.whatsapp.push(data[key])
+      }
+    })
   }
 
   calc(price: number, i: number, value: number) {
@@ -32,6 +62,29 @@ export class PaymentComponent {
     this.totalCost += 3;
   }
 
+  viewRealWatsApp() {
+    if (this.address.valid)
+      this.fakeWhatsappButton = false
+    else
+      this.fakeWhatsappButton = true
+  }
+
+  errorMsg(){
+    this.showAlertMsg=true
+  }
+
+  whatsappDataLinkFun() {
+    let msg = ""
+    this.whatsappDataLink = JSON.parse(localStorage.getItem("products-cart")!) ? JSON.parse(localStorage.getItem("products-cart")!) : [];
+    if (this.whatsappDataLink.length > 1) {
+      for (const temp of this.whatsappDataLink) {
+        msg += `%0A اسم المنتج : ${temp.productsTitle} %0A الكمية المطلوبة : ${temp.productquantity} %0A `;
+      }
+      this.whatsappDataLinkMsg = `مرحبا اريد الحصول علي هذه المنتجات : %0A ${msg} %0A  السعر الكلي شامل خدمة التوصيل  : ${this.totalCost} د.ك `
+    } else if (this.whatsappDataLink.length == 1) {
+      this.whatsappDataLinkMsg = `مرحبا اريد الحصول علي المنتج : %0A اسم المنتج :  ${this.whatsappDataLink[0].productsTitle} %0A الكمية المطلوبة : ${this.whatsappDataLink[0].productquantity} %0A   السعر الكلي شامل خدمة التوصيل  : ${this.totalCost} د.ك`
+    }
+  }
   // ----------------------- delete from cart -----------------------
   delete(itemIndex: number) {
     this.totalCost = 0;
